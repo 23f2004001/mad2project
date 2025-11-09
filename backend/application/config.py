b24c17bd356ff47d758f8f0ac86c2efc9312af04
23f2004001 <23f2004001@ds.study.iitm.ac.in>
2025-11-09 11:57:06 +0530

    controllers commit

--- backend/application/config.py
+++
@@ -1,8 +0,0 @@
-class Config():
-    DEBUG = False
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-class LocalDEvelopmentConfig(Config):
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///vehicleparking.db'
-    JWT_SECRET_KEY = "this-is-a-secret-key"    
